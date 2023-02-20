5c53f77010357cb0fa4e87a00d298dd2ae224b71
p-agboola <peterolamilekan65@gmail.com>
2023-02-20 11:57:41 +0100

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typerOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(typerOrmConfig), AuthModule],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typerOrmConfig),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as config from 'config';
-
-const serverConfig = config.get('server');
-const port = serverConfig.port;
+// import config from 'config';
 
 async function bootstrap() {
+  // const serverConfig = config.get('server');
+
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  
+
+  const port = 3000;
+
   await app.listen(port);
   logger.log(`Application listening on ${port}`);
 }
